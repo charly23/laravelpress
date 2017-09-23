d11e815af6664baf57eb5d6f89352bd25fbd3116
charly23 <charlycapillanes@gmail.com>
2017-09-22 18:04:11 -0700

    admin controller update

--- v5/public/AdminController.php
+++ v5/public/AdminController.php
@@ -42,6 +42,7 @@ use Collective\Html\HtmlBuilder;
 
 // App - Includes
 use App\Includes\DBQuerys;
+use App\Includes\ThumbFeatured;
 
 // http://bensmith.io/email-verification-with-laravel
 
@@ -69,13 +70,14 @@ class AdminController extends Controller
      *
      * @return void
      */
-    public function __construct(Request $request,UrlGenerator $url,MailController $mail,MenuController $menu,DBQuerys $querys)
+    public function __construct(Request $request,UrlGenerator $url,MailController $mail,MenuController $menu,DBQuerys $querys,ThumbFeatured $featured)
     {
         $this->request  = $request;
         $this->url      = $url;
         $this->mail     = $mail;
         $this->menu     = $menu;
         $this->querys   = $querys;
+        $this->featured = $featured;
     }
 
     /**
@@ -138,16 +140,23 @@ class AdminController extends Controller
 
     public function current ($subpage=null) 
     {
+        $in_dashboard = [
+            1 => 'home',
+            2 => 'updates'
+        ];
+
         $in_value = [
             1 => 'add',
             2 => 'edit',
             3 => 'category'
         ];
 
+        $in_key = array_merge($in_dashboard,$in_value);
+
         $end  = end($subpage);
         $ints = is_numeric($end) ? true : false;
 
-        if(in_array($end,$in_value)){
+        if(in_array($end,$in_key)){
             $current = ($subpage[count($subpage)-2]);
         } else if($ints==true) {
             $current = ($subpage[count($subpage)-3]);
@@ -180,12 +189,12 @@ class AdminController extends Controller
 
     public function rows ($id=0) 
     {
-        return DB::table('posts')->where('id',$id)->get();
+        return DB::table('posts')->where(['id'=>$id])->get();
     }
 
     public function meta_value ($id=0) 
     {
-        return DB::table('posts_meta')->where(['posts_id'=>$id])->get();
+        return DB::table('posts_meta')->where(['pid'=>$id])->get();
     }
 
     /**
@@ -204,7 +213,7 @@ class AdminController extends Controller
      *
      * @return objects
      */
-    public function load ($id=null,$validate=null) 
+    public function load ($id=0,$actions=[],$validate=null) 
     {
         $data = $this->request->session()->all();
 
@@ -215,9 +224,10 @@ class AdminController extends Controller
 
         $returns = [
             'index'     =>  $this->i,
-            'id'        =>  $this->intel($subpage),
+            'id'        =>  $id != 0 ? $id : $this->intel($subpage),
             'appconfig' =>  $this->appconfig(),
             'validate'  =>  $validate,
+            'actions'   =>  $actions,
             'page'      =>  $this->page,
             'subpage'   =>  $current,
             'end'       =>  $end,
@@ -230,10 +240,31 @@ class AdminController extends Controller
             'rows'      =>  $this->rows($this->intel($subpage)),
             'meta'      =>  $this->meta($type),
             'meta_value'=>  $this->meta_value($this->intel($subpage)),
-            'querys'    =>  $this->querys
+            'querys'    =>  $this->querys,
+            'featured'  =>  $this->featured
         ];
 
         return $returns;
+    }
+
+    /**
+     * Create a view-id controller load.
+     *
+     * @return void
+     */
+    public function view_id ($page=null,$id=null) 
+    {
+        return view($page,$this->load($id,null,null));
+    }
+
+    /**
+     * Create a view-actions controller load.
+     *
+     * @return void
+     */
+    public function view_actions ($page=null,$actions=[]) 
+    {
+        return view($page,$this->load($this->i,$actions,null));
     }
  
     /**
@@ -243,7 +274,7 @@ class AdminController extends Controller
      */
     public function view ($page=null,$validate=null) 
     {
-        return view($page,$this->load($id=$this->i,$validate));
+        return view($page,$this->load($this->i,null,$validate));
     }
 
     /**
@@ -256,6 +287,15 @@ class AdminController extends Controller
         $html  = null;
         $html .= $this->view('admin/layouts/header');
         $html .= $this->view($page,$validate); 
+        $html .= $this->view('admin/layouts/footer');
+        return $html;
+    }
+
+    public function template_actions ($page=null,$actions=[]) 
+    {
+        $html  = null;
+        $html .= $this->view('admin/layouts/header');
+        $html .= $this->view_actions($page,$actions); 
         $html .= $this->view('admin/layouts/footer');
         return $html;
     }
@@ -326,7 +366,18 @@ class AdminController extends Controller
         if($page == 'admin' AND !is_null($subpage)) { 
             return $this->template('admin/admin');
         } else {
-            return ($type == true) ? $this->template('admin/pages/posts') : $this->template('admin/pages/'.$page);
+            return ($type == true) ? $this->template('admin/pages/posts') : $this->manual($page);
+        }
+    }
+
+    public function manual ($page=null) 
+    {
+        $manual = ['media'];
+        $type = $this->type(true,$page);
+        if($type != true AND in_array($page,$manual)) {
+            return $this->template('admin/pages/'.$page);
+        } else {
+            return $this->template('admin/pages/'.$page);
         }
     }
 
@@ -337,15 +388,16 @@ class AdminController extends Controller
      */
     public function store ()
     {
-        $method = $this->request->method();  
-        $inputs = $this->request->all();
+        $method     = $this->request->method();  
+        $inputs     = $this->request->all();
+        $username   = $this->request->username;
 
         if ($this->request->isMethod('post') AND $this->request->has('_token')) :
             
         $password = $this->request->has('password') ? Hash::make($this->request->password) : null;
 
         $datas = [
-            'username'  => trim($this->request->username),
+            'username'  => trim($username),
             'password'  => $password,
             'email'     => trim($this->request->email),
             'date'      => date("Y-m-d H:i:s")
@@ -366,8 +418,23 @@ class AdminController extends Controller
 
         if ( $validator->fails() != true AND $is_password != false ) {
 
-            DB::table($this->table)->insert($datas); 
-            return redirect('/admin');
+            $exists = $this->autoLogin($username);
+
+            if($exists != false) :
+                return $this->template('admin/interface/register',["Username - Already Exists ({$username})"]);
+            endif;
+
+            if($exists != true) :
+
+                foreach($inputs as $keys => $vals) : if ($this->request->has($keys)) :
+                        $this->request->session()->put($keys,$vals);
+                    endif;
+                endforeach;
+
+                DB::table($this->table)->insert($datas); 
+                return redirect('/admin');
+
+            endif;
 
         } else {
 
@@ -385,16 +452,17 @@ class AdminController extends Controller
      */
     public function login ()
     {
-        $method = $this->request->method();
-        $inputs = $this->request->all();
+        $method     = $this->request->method();
+        $inputs     = $this->request->all();
+        $username   = $this->request->username;
 
         if ($this->request->isMethod('post') AND $this->request->has('_token'))  :
-            
+        
         $password = Hash::make($this->request->password);
 
         $datas = [
-            'username' => trim($this->request->username),
-            'password' => trim($this->request->password)
+            'username' => trim($username),
+            'password' => $this->request->password
         ];
 
         $rules = [
@@ -405,29 +473,50 @@ class AdminController extends Controller
         $validator = Validator::make($datas,$rules);
         $errors = $validator->errors();
 
-        $is_password = Hash::check($this->request->password,$password) ? true : false;
+        $querys = $this->querys->get_users_by_name($username);
+        $password_validate = isset($querys->password) ? trim($querys->password) : null;
+
+        $is_password = Hash::check($this->request->password,$password_validate) ? true : false;
         $confirmation_code = str_random(30);
         
         if ( $validator->fails() != true AND $is_password != false ) {
 
-            $querys = DB::table($this->table)->where('username',trim($this->request->username))->get();
+            $exists = $this->autoLogin($username);    
 
-            foreach($inputs as $keys => $vals) : if ($this->request->has($keys)) :
-                    $this->request->session()->put($keys,$vals);
+            if($exists != false) :
+
+                foreach($inputs as $keys => $vals) : if ($this->request->has($keys)) :
+                        $this->request->session()->put($keys,$vals);
+                    endif;
+                endforeach;
+                
+                if ($this->request->session()->has('username') AND $this->request->session()->has('password')) :
+                    return redirect('/admin');
                 endif;
-            endforeach;
 
-            if ($this->request->session()->has('username') AND $this->request->session()->has('password')) {
-                return redirect('/admin');
-            }
+            endif;
+
+            if($exists != true) :
+                return $this->template('admin/interface/login',["Username - Not Exists ({$username})"]);
+            endif;
 
         } else {
 
-            $message = $errors->all();
+            $message = count($errors->all()) !=0 ? $errors->all() : ['Password - Wrong'];
             return $this->template('admin/interface/login',$message);
         }
 
         endif;
+    }
+
+    /**
+     * application auto-login - exists validate.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function autoLogin ($username=null) {
+        return $this->querys->get_users_by_name($username) ? true : false;
     }
 
     /**
@@ -456,91 +545,138 @@ class AdminController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function action () 
+    public function action ($page=null,$subpage=null,$tab=null) 
     {
         $inputs = $this->request->all();
         $sessions = $this->request->session()->all();
 
+        /**
+         * ajaxs-handler
+         * javascripts-ajaxs action-handler
+        **/
         if ($this->request->isMethod('post') AND $this->request->has('_token') AND $this->request->action == 'posts-action' )  :
-
-            $ids    = ($this->request->id);
-            $fields = ($this->request->fields);
-            $metas  = ($this->request->meta);
-
-            if(!is_null($fields)) : foreach($fields as $keys => $vals) : 
-                    $name         = $vals['name'];
-                    $datas[$name] = $vals['value'];
-                    $rules[$name] = 'required';
-                endforeach;
-            endif;
-
-            $author = [
-                'status'    => 1,
-                'author'    => 0,
-                'date'      => date("Y-m-d H:i:s")    
-            ];
-
-            $merge = array_merge($datas,$author);
-            
-            $messages = [
-                'title.required' => 'The (:attribute) field is required',
-            ];
-
-            $validator = Validator::make($datas,$rules,$messages);
-            $errors = $validator->errors(); 
-
-            if($validator->fails() != true AND is_array($merge)) :
-
-                $metas_id = $this->meta_insert($ids,$metas);
-
-                if($ids==0) :
-
-                    $posts_id = DB::table('posts')->insertGetId($merge);
-                
-                    $return = [
-                        'posts_id'  => $posts_id,
-                        'metas_id'  => $metas_id
-                    ];
-
-                else :
-
-                    DB::table('posts')->where('id',$ids)->update($merge);
-
-                    $return = [
-                        'posts_id'  => $ids,
-                        'metas_id'  => $metas_id
-                    ];
-
-                endif;
-
-                return response()->json($return);
-
-            endif;
-
-            if($validator->fails() != false) :
-                $message = $errors->all();
-                return $this->view('admin/pages/posts/validate',$message);
-            endif;
-
+            return $this->postsInsert();
         endif;
-
-        if (!$this->request->session()->has('username')) {
-            return redirect('/admin');
-        }
 
         if ($this->request->isMethod('post') AND $this->request->has('_token') AND $this->request->action == 'meta-action' )  :
             return $this->view('admin/pages/posts/meta');
         endif;
 
+        if ($this->request->isMethod('post') AND $this->request->has('_token') AND $this->request->action == 'media-action' )  :
+            $media_id = intval($inputs['id']);
+            return $this->view_id('admin/pages/media/browse',$media_id);
+        endif;
+
+        if ($this->request->isMethod('post') AND $this->request->has('_token') AND $this->request->action == 'media-thumbnail' )  :
+            $media_id = intval($inputs['id']);
+            return $this->view_id('admin/pages/media/selected',$media_id);
+        endif;
+
+        /**
+         * actions-handler
+         * phpscripts-load action-handler
+        **/
+        if ($this->request->isMethod('post') AND $this->request->has('_token') AND $this->request->has('media-action') )  :
+            return $this->mediaInsert($tab);
+        endif;
+
+        if ($this->request->isMethod('post') AND $this->request->has('_token') AND $this->request->has('filter-submit') )  :
+            return $this->postsFilter($page);
+        endif;
+
+        if (!$this->request->session()->has('username')) :
+            return redirect('/admin');
+        endif;
+
         die();
     }
 
-    public function meta_insert ($id=null,$meta=null) 
+    /**
+     * application event - action function (posts-insert).
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function postsInsert () 
+    {
+        $dval   = null;
+        $ids    = intval($this->request->id);
+        $mid    = intval($this->request->mid);
+        $fields = ($this->request->fields);
+        $dates  = ($this->request->date);
+        $metas  = ($this->request->meta);
+
+        if(!is_null($fields)) : foreach($fields as $keys => $vals) : $name = $vals['name'];
+                $datas[$name] = $vals['value'];
+                $rules[$name] = 'required';
+            endforeach;
+        endif;
+
+        if(!is_null($dates)) : foreach($dates as $dkeys => $dvals) :
+               var_dump($dvals['value']);
+            endforeach;
+        endif;
+
+        $author = [
+            'status'    => 1,
+            'mid'       => $mid,
+            'author'    => 0,
+            'date'      => date("Y-m-d H:i:s")    
+        ];
+
+        $merge = array_merge($datas,$author);
+        
+        $messages = [
+            'title.required' => 'The (:attribute) field is required',
+        ];
+
+        $validator = Validator::make($datas,$rules,$messages);
+        $errors = $validator->errors(); 
+
+        if($validator->fails() != true AND is_array($merge)) :
+
+            $metas_id = $this->metaInsert($ids,$metas);
+
+            if($ids==0) :
+
+                $posts_id = DB::table('posts')->insertGetId($merge);
+            
+                $return = [
+                    'pid'  => $posts_id,
+                    'mid'  => $metas_id
+                ];
+
+            else :
+
+                DB::table('posts')->where('id',$ids)->update($merge);
+
+                $return = [
+                    'pid'  => $ids,
+                    'mid'  => $metas_id
+                ];
+
+            endif;
+
+            return response()->json($return);
+
+        endif;
+
+        if($validator->fails() != false) :
+            $message = $errors->all();
+            return $this->view('admin/pages/posts/validate',$message);
+        endif;
+    }
+
+    /**
+     * application event - action function (meta-insert).
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function metaInsert ($id=null,$meta=null) 
     {
         $metas = [];
         if(count($meta)>=0 AND is_array($meta)) : foreach($meta as $keys => $vals) :
                 $mids = $vals['id'];
-                $data = array_merge($vals,['posts_id'=>$id]);
+                $data = array_merge($vals,['pid'=>$id]);
                 unset($data['id']);
                 if($mids==0) :
                     $ids = DB::table('posts_meta')->insertGetId($data);
@@ -553,4 +689,101 @@ class AdminController extends Controller
         endif; 
         return $metas;
     }
+
+    /**
+     * application event - action function (media-insert).
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function mediaInsert ($id=null) 
+    {
+        $files  = $this->request->file('media-file');
+        $fields = $this->request->all();
+
+        $avl = [
+            '_token',
+            'id',
+            'media-action'
+        ];
+
+        if(!is_null($files) && count($files)>=0 || is_array($files)) : 
+
+            foreach($files as $keys => $vals):
+
+                $names = time().$keys.'.'.$vals->getClientOriginalExtension();
+                $paths = storage_path('app/public/uploads/'.date('Y/n'));
+
+                $data = [
+                    'name'  => $names,
+                    'path'  => $paths,
+                    'description' => $this->request->description,
+                    'pid'   => 0,  
+                    'author'=> 0,
+                    'date'  => date("Y-m-d H:i:s")
+                ];
+
+                if(!is_null($id)) {
+                    $action = DB::table('media')->where('id',$id)->update($data);
+                } else {
+                    $action = DB::table('media')->insert($data);
+                }
+
+                if($action) :
+                    $vals->move($paths,$names);
+                endif;
+
+            endforeach;
+
+        else :
+
+            if(!is_null($fields)) : foreach($fields as $keys => $vals) : 
+                        if(!in_array($keys,$avl)) : $data[$keys] = $vals;
+                    endif;
+                endforeach;
+            endif;
+            DB::table('media')->where('id',$id)->update($data);
+
+        endif;
+
+        if(!is_null($id)) {
+            return redirect('/admin/media/edit/'.$id);
+        } else {
+            return redirect('/admin/media');
+        }
+
+    }
+
+    /**
+     * application event - filter function (filter-action).
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function postsFilter ($page=null) 
+    {
+        $actions = $this->request->action;
+        $searchs = $this->request->search;
+
+        if ($actions == 0 AND !is_null($searchs))  :
+
+            $filters = [
+                'action' => $actions,
+                'search' => $searchs
+            ];
+
+            return $this->template_actions('admin/pages/posts',$filters);
+
+        elseif ($actions == 1 AND is_null($searchs)) :
+
+            return $this->template('admin/pages/posts');
+
+        elseif ($actions == 2 AND is_null($searchs)) :
+
+        else :
+
+            return $this->template('admin/pages/posts');
+
+        endif;
+    }
+
+    // END
 }
